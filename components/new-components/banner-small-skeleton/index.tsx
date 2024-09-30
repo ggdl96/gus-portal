90c4f366efe579b8fea011e09db5c66aec610d78
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2024-09-30 13:33:26 -0300

    improving styles

--- components/new-components/banner-small-skeleton/index.tsx
+++ components/new-components/banner-small-skeleton/index.tsx
@@ -11,7 +11,7 @@ export const BannerSmallSkeleton = () => {
   const data = [1, 2, 3];
 
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.wrapper}>
       <ScrollView horizontal={true}>
         {data.map((item) => {
           return (
--- components/new-components/banner-small-skeleton/styles.tsx
+++ components/new-components/banner-small-skeleton/styles.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  wrapper: { flex: 1 },
   container: {
     backgroundColor: '#292929',
   },
