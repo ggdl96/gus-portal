02674619045b062b3e3d8fcd5501ced964761d79
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2024-10-02 11:00:39 -0300

    add styles constants

---
+++ styles/borders/index.ts
@@ -0,0 +1,14 @@
+const borders = {
+  radius: {
+    small: 4,
+    medium: 6,
+    high: 8,
+  },
+  sizes: {
+    small: 1,
+    medium: 2,
+    high: 4,
+  },
+};
+
+export default borders;
---
+++ styles/colors/index.ts
@@ -0,0 +1,32 @@
+const colors = {
+  contrastPrimary: {
+    10: '#191712',
+    20: '#27241D',
+    80: '#504B3E',
+    90: '#554F42',
+    100: '#595345',
+    200: '#7C7461',
+    300: '#958B75',
+    400: '#AA9E86',
+    500: '#BCAE94',
+  },
+  contrastSecondary: {
+    950: '#F8F9F7',
+    900: '#F2F3F1',
+    800: '#E6E7E5',
+    700: '#D9DAD8',
+    600: '#CACBCA',
+    500: '#BBBCBB',
+  },
+  primary: {
+    650: '#52D394',
+  },
+  secondary: {
+    650: '#D3680C',
+  },
+  tertiary: {
+    650: '#D31415',
+  },
+} as const;
+
+export default colors;
