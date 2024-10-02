75dbbaf32d825ccfacf2f29f29710bdad9b81b13
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2024-10-02 11:01:17 -0300

    add mock data for medium and detailed banners

--- __mocks__/screens/home/index.ts
+++ __mocks__/screens/home/index.ts
@@ -68,3 +68,27 @@ const smallA3 = {
 export const TOP_BANNERS_DATA = [dataAux, dataAux2, dataAux3];
 
 export const SMALL_BANNERS_DATA = [smallA1, smallA2, smallA3];
+
+export const MEDIUM_BANNERS_DATA = [
+  {
+    categories: ['cat1'],
+    text: 'some text',
+    image:
+      'https://images.pexels.com/photos/159866/books-book-pages-read-literature-159866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  },
+];
+
+export const DETAILED_BANNERS_DATA = [
+  {
+    owner: {
+      id: 'bbcvbv55hs5s21c',
+      name: 'ghfh',
+      image:
+        'https://images.pexels.com/photos/1743165/pexels-photo-1743165.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+    id: 'ty453gdfgdfg456gd5',
+    image:
+      'https://images.pexels.com/photos/1743165/pexels-photo-1743165.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    title: 'Test',
+  },
+];
