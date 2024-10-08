3867aa4ac915614986dbfbee474555f35110a950
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2024-10-08 18:08:33 -0300

    add logic to restric redirection of TitleWithAvatar

--- components/new-components/title-with-avatar/index.tsx
+++ components/new-components/title-with-avatar/index.tsx
@@ -3,18 +3,21 @@ import { GestureResponderEvent, TouchableOpacity, View, ViewStyle } from 'react-
 import styles from './styles';
 import Avatar from '../avatar';
 import BannerTitle from '../banner-title';
+import { router } from 'expo-router';
 
 interface Props {
   title: string;
   src?: { uri: string };
   style?: ViewStyle;
   id?: string;
+  shouldRedirect?: boolean;
 }
 
-const TitleWithAvatar = ({ title, src, style, id }: Props) => {
+const TitleWithAvatar = ({ title, src, style, id, shouldRedirect = true }: Props) => {
   const _handleOnPress = (e: GestureResponderEvent) => {
-    if (id) {
+    if (id && shouldRedirect) {
       e.stopPropagation();
+      router.navigate(`/seller/${id}`);
     }
   };
 
