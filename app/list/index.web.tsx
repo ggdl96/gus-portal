746cdfba090aff56424f1ba1ea6c7e93dd1e1eb5
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2024-10-31 11:14:58 -0300

    add pagination display in list screen for web

--- app/list/index.web.tsx
+++ app/list/index.web.tsx
@@ -10,6 +10,7 @@ import { useWindowDimensions, View } from 'react-native';
 import { screens } from '@/styles/screens';
 import { useLocalSearchParams } from 'expo-router';
 import SearchListHead from '@/components/new-components/search-list-head';
+import BannerSubTitle from '@/components/new-components/banner-subtitle';
 
 export default function Index() {
   const dimensions = useWindowDimensions();
@@ -37,6 +38,15 @@ export default function Index() {
             key={`product_${item.id}`}
           />
         ))}
+        <View className="flex flex-row w-full p-4">
+          <View className="flex flex-row border-t-2 border-t-contrastPrimary-80 w-full justify-center pb-2">
+            {['1', '2'].map((item) => (
+              <View className="p-2">
+                <BannerSubTitle title={item} />
+              </View>
+            ))}
+          </View>
+        </View>
       </View>
     </LayoutBasic>
   );
